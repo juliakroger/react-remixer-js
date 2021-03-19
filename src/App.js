1eef26ff91d4b4b54cf863cd16a1b210ddc653b2
Julia Kroger <julia.kroger@gmail.com>
2021-03-19 08:29:49 -0300

    feat: main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
 import "./App.css";
+import SimpleRemixerAnimation from "./remixers/SimpleRemixerAnimation";
+import SelectRemixerAnimate from "./remixers/SelectRemixerAnimate";
+import SelectRemixerAnimateIn from "./remixers/SelectRemixerAnimateIn";
 
-function App() {
-  return <div className="App">Learn react remix js</div>;
-}
+const App = () => {
+  return (
+    <div className="cards-row">
+      <SimpleRemixerAnimation />
+      <SelectRemixerAnimate />
+      <SelectRemixerAnimateIn />
+    </div>
+  );
+};
 
 export default App;
