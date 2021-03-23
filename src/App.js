ef6984163e662312ed5fbe5b5d36b628c768d110
Julia Kroger <julia.kroger@gmail.com>
2021-03-23 14:12:52 -0300

    feat: add card styles using remixer styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import Remixer from "@siteremix/remixer";
 import SimpleRemixerAnimation from "./remixers/SimpleRemixerAnimation";
 import SelectRemixerAnimate from "./remixers/SelectRemixerAnimate";
 import SelectRemixerAnimateIn from "./remixers/SelectRemixerAnimateIn";
 import SimpleRemixerAnimationDisableAndEnable from "./remixers/SimpleRemixerAnimationDisableAndEnable";
+import "./App.css";
 
 const App = () => {
+  useEffect(() => {
+    new Remixer(".card")
+      .styles({
+        margin: "10px",
+        padding: "10px",
+        width: "300px",
+        height: "300px",
+        backgroundColor: "#20232a",
+        borderRadius: "10px",
+      })
+      .run();
+  }, []);
+
   return (
     <div className="cards-row">
       <SimpleRemixerAnimation />
